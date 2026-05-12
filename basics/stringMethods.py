847bc08c07bf8aaf60fc8aac3c81390c8a6b0a7e
Coding With Brian <CodingWithBrian77@gmail.com>
2026-05-12 10:32:07 -0400

    Finished string methods file

---
+++ basics/stringMethods.py
@@ -0,0 +1,14 @@
+# name = input("Enter your full name: ")
+# phone_number = input("Enter your phone number: ")
+# result = len(name) #len() gives length of a string including spaces
+# result = name.find(" ") #.find() gives location or index of first occurance of a given character, returns -1 if nothing found
+# result = name.rfind(" ") #similar to find but in reverse, so the last occurance, returns -1 if nothing found
+# name = name.capitalize() #capitalizes first letter of a string
+# name = name.upper() #all letters become uppercased
+# name = name.lower() #all letters become lowercased
+# result = name.isdigit() #returns True if string only contains digits, so no mixes or spaces
+# result = name.isalpha() #returns True if string only contains alphabet letters, so not mixes or spaces
+# result = phone_number.count("-")
+# phone_number = phone_number.replace("-", " ")
+# print(help(str)) can use this if confused or cant remember string methods
+# print(phone_number)
