05cc2ac4ff71f5e9f06be2cf42882f139215db74
Coding With Brian <CodingWithBrian77@gmail.com>
2026-05-13 09:56:03 -0400

    Added and fiinished for loops file

---
+++ basics/forLoops.py
@@ -0,0 +1,23 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+# for x in range(1, 11): # x represents a counter
+    # print(x) # outputs 1 2 3 4 5 6 7 8 9 10
+
+# for x in reversed(range(1, 11)):
+    # print(x) # outputs 10 9 8 7 6 5 4 3 2 1 HAPPY NEW YEAR!
+# print("HAPPY NEW YEAR!")
+
+# for x in range(1, 11, 2):
+    # print(x) #outputs 1 3 5 7 9
+
+# credit_card = "1234-5678-9012-3456"
+# for x in credit_card:
+    # print(x) #outputs each individual character of the string on a new line
+
+# for x in range(1, 21):
+    # if x == 13:
+        # continue #skips over that iteration
+    # else:
+        # print(x) #outputs 1 - 20, excluding 13
+
