b9dc733966639d49c0ab75691ce54cfb7a7c6f0a
Coding With Brian <CodingWithBrian77@gmail.com>
2026-05-13 09:38:34 -0400

    Added and finished while loop file

---
+++ basics/whileLoops.py
@@ -0,0 +1,29 @@
+# while loop = execute some code WHILE some condition remains true
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# food = input("Enter a food you like (q to quit): ")
+# num = int(input("Enter a # between 1-10: "))
+# while name == "":
+    # print("You did not enter your name")
+    # name = input("Enter your name: ")
+    # there must be some way to exit the loop, otherwise it's an infinite loop
+# print(f"Hello {name}")
+
+# while age < 0:
+    # print("age can't be negative")
+    # age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old")
+
+# while not food == "q":
+    # print(f"You like {food}")
+    # food = input("Enter another food you like (q to quit): ")
+
+# print("bye")
+
+# while num < 1 or num > 10:
+    # print(f"{num} is not valid")
+    # num = int(input("Enter a # between 1 - 10: "))
+
+# print(f"Your number is {num}")
