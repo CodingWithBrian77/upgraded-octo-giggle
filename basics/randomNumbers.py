55a3f7875a5e60808f15aa8d20c95ffc91a48d64
Coding With Brian <codingwithbrian77@gmail.com>
2026-05-14 10:13:03 -0400

    Added and finished random number file

---
+++ basics/randomNumbers.py
@@ -0,0 +1,13 @@
+import random # gives access to a bunch of methods revolving around random
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+# number = random.randint(1, 6) # generates a random number from 1-6
+# number = random.randint(low, high) # can use variables that hold int values
+# number = random.random() # generates a random floating point number in between 0 and 1
+# option = random.choice(options) # randomly chooses a value in between a given collection set with given values
+# random.shuffle(cards) # shuffles a collection set's values
+# print(cards)
