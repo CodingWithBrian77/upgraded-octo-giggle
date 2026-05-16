94e2decb575320611f2c2649eb4e2f538b4b32d2
Coding With Brian <codingwithbrian77@gmail.com>
2026-05-16 09:28:47 -0400

    Added and finished __name__ file and 2 script files

---
+++ basics/__name__.py
@@ -0,0 +1,16 @@
+# if __name__ == __main__: (this script can be imported or run standalone)
+#                          Functions and classes in this module can be reused
+#                          without the main block of code executing
+# Good practice (code is modular, helps readability,
+#                leaves no global variables,
+#                avoid unintended execution)
+#
+#    ex. library = Import library for functionality
+#                  When running library diretly, display a help page
+
+def main():
+    # program here
+
+# if __name__ == '__main__':
+    # main()
+
