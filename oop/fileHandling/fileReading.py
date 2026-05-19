a2a7609d26a36f3c7426e4c67e3946cbd8daacc2
Coding With Brian <codingwithbrian77@gmail.com>
2026-05-19 11:04:51 -0400

    Added and finished file reading file, added 3 input files to read from

--- oop/fileHandling/fileReading.py
+++ oop/fileHandling/fileReading.py
@@ -1,1 +1,36 @@
 # Python reading files (.txt, .json, .csv)
+ 
+import json
+import csv
+
+file_path1 = "oop/fileHandling/input.txt"
+file_path2 = "oop/fileHandling/input.json"
+file_path3 = "oop/fileHandling/input.csv"
+
+try:
+    with open(file_path1, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
+
+try:
+    with open(file_path2, "r") as file:
+        content = json.load(file)
+        print(content["job"])
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
+
+try:
+    with open(file_path3, "r") as file:
+        content = csv.reader(file)
+        for line in content:
+            print(line[2])
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to read that file")
