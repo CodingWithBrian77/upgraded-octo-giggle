4c4de8ce773947efe7c57f5cc909512bce8ac527
Coding With Brian <codingwithbrian77@gmail.com>
2026-05-19 10:24:25 -0400

    Added and finished file detection file with a test.txt file

---
+++ oop/oopIntro/fileHandling/fileDetection.py
@@ -0,0 +1,14 @@
+# Python file detection
+
+import os # imports operating system module
+
+file_path = "oop/oopIntro/fileHandling" # relative file path
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a folder/directory")
+else:
+    print("That location doesn't exist")
